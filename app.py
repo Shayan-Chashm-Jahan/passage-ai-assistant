b61adb4cce996c8732cb055dd8a28e6aa75d3322
Shayan-Chashm-Jahan <shayan.chashmjahan@gmail.com>
2024-06-20 23:01:36 -0400

    Removing matchbuddy

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ SPLITTER_API_KEY = None
 splitter_client = None
 splitter_instructions = None
 
-MATCH_BUDDY_API_KEY = None
-matchbuddy_client = None
-matchbuddy_instructions = None
-
 document_embeddings = None
 document_chunks = None
 
@@ -38,12 +34,6 @@ def initialize():
     SPLITTER_API_KEY = st.secrets["SPLITTER_OPENAI_API_KEY"]
     splitter_client = OpenAI(api_key=SPLITTER_API_KEY)
     splitter_instructions = load_funcs.load_instructions("splitter_instructions.txt")
-
-    global MATCH_BUDDY_API_KEY, matchbuddy_client, matchbuddy_instructions
-
-    MATCHBUDDY_API_KEY = st.secrets["MATCHBUDDY_API_KEY"]
-    matchbuddy_client = OpenAI(api_key=MATCH_BUDDY_API_KEY)
-    matchbuddy_instructions = load_funcs.load_instructions("matchbuddy_instructions.txt")
 
     global tools
     
@@ -76,9 +66,6 @@ if 'ella_conversation_history' not in st.session_state:
 if 'splitter_conversation_history' not in st.session_state:
     st.session_state.splitter_conversation_history = [{"role": "system", "content": splitter_instructions}]
 
-if 'matchbuddy_conversation_history' not in st.session_state:
-    st.session_state.matchbuddy_conversation_history = [{"role": "system", "content": matchbuddy_instructions}]
-
 if 'response_dict' not in st.session_state:
     st.session_state.response_dict = {"response": "", "follow-up": []}
 
@@ -92,7 +79,6 @@ def handle_user_input(user_input=None):
 
     if user_input:
         st.session_state.splitter_conversation_history.append({"role": "user", "content": user_input})
-        st.session_state.matchbuddy_conversation_history.append({"role": "user", "content": user_input})      
         st.session_state.ella_conversation_history.append({"role": "user", "content": user_input})
 
         parts = utils.question_parts(splitter_client, st.session_state.splitter_conversation_history)
